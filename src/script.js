918c73b6a5995df83d80bd9bcaf158c3cae04ddf
leuxtc <l3oxon3@gmail.com>
2024-11-11 18:39:39 -0300

    Finalizando projeto

---
+++ src/script.js
@@ -0,0 +1,59 @@
+const emojis = [
+    "😎",
+    "😎",
+    "😉",
+    "😉",
+    "😊",
+    "😊",
+    "❤️",
+    "❤️",
+    "✌️",
+    "✌️",
+    "👍",
+    "👍",
+    "🫡",
+    "🫡",
+    "🤨",
+    "🤨"
+]
+
+let openCards = [
+
+]
+
+let randomEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1))
+
+for (i = 0; i < emojis.length; i++){
+    let box = document.createElement("div")
+    box.className = "item"
+    box.onclick = handleClick
+    box.innerHTML = randomEmojis[i]
+    document.querySelector(".game").appendChild(box)
+}
+
+function handleClick() {
+    if (openCards.length < 2){
+        this.classList.add("boxOpen")
+        openCards.push(this)
+    }
+
+    if (openCards.length == 2){
+        setTimeout(checkMatch, 500)
+    }
+}
+
+function checkMatch() {
+    if(openCards[0].innerHTML === openCards[1].innerHTML) {
+        openCards[0].classList.add("boxMatch")
+        openCards[1].classList.add("boxMatch")
+    } else {
+        openCards[0].classList.remove("boxOpen")
+        openCards[1].classList.remove("boxOpen")
+    }
+
+    openCards = []
+
+    if(document.querySelectorAll(".boxMatch").length === emojis.length){
+        alert("Parabéns, você encontrou todos emojis!")
+    }
+}
